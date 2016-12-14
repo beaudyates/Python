ad0ecce81c960c6979b23588497d5a9a468781d3
Beau Yates <beauyates1977@gmail.com>
2016-12-13 19:07:20 -0800

    added range drill
    the Python course drill that introduces the range() function

---
+++ Python_range_drill.py
@@ -0,0 +1,30 @@
+'''Start IDLE and use the Python range() function with one parameter to display the
+following:
+0
+1
+2
+3'''
+
+for i in range(4):
+    print(i)
+print("\n")
+
+'''Use the Python range() function with 3 parameters to display the following:
+3
+2
+1
+0'''
+
+for i in range(3, -1, -1):
+    print(i)
+print("\n")
+
+'''Use the Python range() function with 3 parameters to display the following:
+8
+6
+4
+2'''
+
+for i in range(8, 0, -2):
+    print(i)
+
